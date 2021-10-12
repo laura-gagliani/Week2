4640ce61290846a3850298fe0a663c377b8d45c5
Laura Gagliani <gaglianilaura@gmail.com>
2021-10-12 09:59:41 +0200

    esercizio bolletta

--- Day1011.EsercizioBolletta/Program.cs
+++ Day1011.EsercizioBolletta/Program.cs
@@ -6,7 +6,6 @@ namespace Day1011.EsercizioBolletta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             //Realizzare un’applicazione console che consenta di eseguire il calcolo della bolletta della luce.
             //Si richiede di sviluppare un menù attraverso cui l’utente può scegliere di:
 
@@ -17,14 +16,91 @@ namespace Day1011.EsercizioBolletta
 
             Console.WriteLine("*****Benvenuto sul sito Luce & Energia*****");
 
-            Console.WriteLine("Seleziona dal menù:");
-            Console.WriteLine("(1) Effettua l'accesso");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("(1) Inserimento dati");
             Console.WriteLine("(2) Calcola il costo della tua bolletta");
             Console.WriteLine("(3) Riepilogo dati");
+            
+            Console.WriteLine("\nDigita il numero corrispondente alla voce desiderata");
+            bool choice = int.TryParse(Console.ReadLine(), out int menuChoice);
+            while ((!choice) && menuChoice < 1 && menuChoice > 3)
+            {
+                Console.WriteLine("\nErrore nell'inserimento. Riprova");
+                choice = int.TryParse(Console.ReadLine(), out menuChoice);
 
-            //selezione numerica con tryparse
+            }
+            bool login = false;
+            int consumo = 0;
+            int costo = 0;
+            string nomeUtente = null;
+            string cognomeUtente = null;
+
+            //in questo caso un metodo con out sarebbe stato "poco sensato", perché comunque la presenza
+            //dello switch case rende obbligatorio il dichiarare le variabili all'inizio. 
+            //cambiato con by ref (la variabile è inizializzata, il metodo ci va a sovrascrivere)
+
+            switch (menuChoice)
+            {
+                case 1:
+                    login = LogIn(ref consumo, ref nomeUtente, ref cognomeUtente);
+                    break;
+                case 2:
+                    if (!login) {
+                        Console.WriteLine("Attenzione! Dati non inseriti");
+                        login = LogIn(ref consumo, ref nomeUtente, ref cognomeUtente);
+                    }
+                    costo = CalcolaBolletta(consumo);
+                    Console.WriteLine($"La tua bolletta ha un costo di {costo} euro");
+                    break;
+                case 3:
+                    if (!login)
+                    {
+                        Console.WriteLine("Attenzione! Dati non inseriti");
+                        login = LogIn(ref consumo, ref nomeUtente, ref cognomeUtente);
+                    }
+                    costo = CalcolaBolletta(consumo);
+                    Riepiloga(consumo, costo, nomeUtente, cognomeUtente);
+                    break;
+            }
+            
 
 
+        }
+
+        private static bool LogIn(ref int consumo, ref string nomeUtente, ref string cognomeUtente)
+        {
+            bool login;
+            Console.WriteLine("*****Log In*****");
+            Console.WriteLine("\nNome:");
+            nomeUtente = Console.ReadLine();
+            Console.WriteLine("\nCognome:");
+            cognomeUtente = Console.ReadLine();
+            Console.WriteLine("\nkwH consumati:");
+            bool consumoCorretto = int.TryParse(Console.ReadLine(), out  consumo);
+            while (!consumoCorretto)
+            {
+                Console.WriteLine("\nErrore. Reinserire il valore:");
+                consumoCorretto = int.TryParse(Console.ReadLine(), out consumo);
+            }
+
+            Console.WriteLine("\nDati inseriti con successo");
+
+            return login = true;
+        }
+
+        private static int CalcolaBolletta(int consumo)
+        {
+            int costo = 40 + (consumo * 10);
+            return costo;
+        } 
+
+        private static void Riepiloga(int consumo, int costo, string nomeUtente, string cognomeUtente)
+        {
+            Console.WriteLine("\n*****Riepilogo Dati*****\n");
+            Console.WriteLine($"Nome: \t\t{nomeUtente}");
+            Console.WriteLine($"Cognome: \t{cognomeUtente}");
+            Console.WriteLine($"Consumo: \t{consumo} kwH");
+            Console.WriteLine($"Costo bolletta: {costo} euro");
         }
     }
 }
