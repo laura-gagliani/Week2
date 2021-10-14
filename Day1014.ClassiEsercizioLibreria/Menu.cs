839378dbe12cddebff68903c445645e81a6e8bdf
Laura Gagliani <gaglianilaura@gmail.com>
2021-10-14 10:31:51 +0200

    wip

---
+++ Day1014.ClassiEsercizioLibreria/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1014.ClassiEsercizioLibreria
+{
+    public static class Menu        //scriviamo anche "static" qua
+                                    //questa contiene un metodo di fatto
+                                    //menu non è un oggetto, non creo istanze menu1, menu2, menu3 come per la classe libri
+                                    //per questo è static
+                                    //comunque posso accedere alla classe dal main del program
+    {
+        public static void Start()
+        {
+            bool nuovaOperazione = true;
+            Console.WriteLine("------------Benvenuto!------------\n");
+            do
+            {
+                Console.WriteLine("Premi 1 per aggiungere un libro");
+                Console.WriteLine("Premi 2 per eliminare un libro");
+                Console.WriteLine("Premi 3 per modificare un libro");
+                Console.WriteLine("Premi 4 per stampare tutti i libri");
+                Console.WriteLine("Premi 5 per estrarre i libri per genere");
+                Console.WriteLine("Premi 0 per uscire");
+
+                int scelta;
+                bool sceltaCorretta = int.TryParse(Console.ReadLine(), out scelta);
+
+                while ((!sceltaCorretta) || scelta < 0 || scelta > 5)
+                {
+                    Console.WriteLine("Inserimento errato. Riprova");
+                    sceltaCorretta = int.TryParse(Console.ReadLine(), out scelta);
+                }
+
+                switch (scelta)
+                {
+                    case 1:
+                        //TODO aggiungi libro
+                        break;
+                    case 2:
+                        //TODO togli libro
+                        break;
+                    case 3:
+                        //TODO modifica libro
+                        break;
+                    case 4:
+                        //TODO stampa tutti libri
+                        break;
+                    case 5:
+                        //TODO filtra per genere
+                        break;
+                    case 0:
+                        Console.WriteLine("Arrivederci!");
+                        nuovaOperazione = false;
+                        break;
+                }
+                                
+            } while (nuovaOperazione);
+
+
+        }
+    }
+}
--- Day1014.ClassiEsercizioLibreria/Program.cs
+++ Day1014.ClassiEsercizioLibreria/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day1014.ClassiEsercizioLibreria
 {
@@ -6,6 +7,7 @@ namespace Day1014.ClassiEsercizioLibreria
     {
         static void Main(string[] args)
         {
+
             #region traccia
             //Scrivere un programma che gestisca una libreria.
 
@@ -22,15 +24,49 @@ namespace Day1014.ClassiEsercizioLibreria
             //Sarà possibile inserire un nuovo libro, eliminare un libro, modificare un libro o cercare i libri per genere
             #endregion
 
-            Libro libroTot = new Libro(); //dove Libro() è il costruttore.
+            Menu.Start();       //quando chiamo il metodo Start della classe Menu lui salta all'altro file e va a vedere cosa fare
+
+
+            #region costruzione di oggetti
+            Libro libro1 = new Libro(); //dove Libro() è il costruttore.
                                           //io sto dicendo costruiscimi una oggetto esempioDiLibro del "tipo" Libro, la classe che ho costruito
                                           //il costruttore prepara un pezzo di memoria per metterci tutti i dati che abbiamo preparato per la classe
 
-            libroTot.Codice = "Codice001alfanumerico"; //nome.Proprietà
-            libroTot.Autore = "Collodi";
-            libroTot.Titolo = "Pinocchio";
-            libroTot.Prezzo = 12.20;
-            libroTot.DataPubblicazione = new DateTime(1880);
+            //assegnazione dei valori alle proprietà
+            libro1.Codice = "Codice001alfanumerico"; //nome.Proprietà
+            libro1.Autore = "Collodi";
+            libro1.Titolo = "Pinocchio";
+            libro1.Prezzo = 12.20;
+            libro1.DataPubblicazione = new DateTime(1883);
+
+            Console.WriteLine(libro1.Autore);
+
+            Libro libro2 = new Libro();
+
+            libro2.Codice = "jefkksj3t4tkfd";
+            libro2.Autore = "Alessandro Manzoni";
+            libro2.Prezzo = 9.99;
+            //e così via
+            #endregion
+
+            #region lista
+            List<Libro> listaDiLibri = new List<Libro>(); //creo una lista che può contenere oggetti "Libro"
+                                                          //non devo specificare la dimensione
+
+            listaDiLibri.Add(libro1);
+            Console.WriteLine($"La lista contiene {listaDiLibri.Count} libri");
+            listaDiLibri.Add(libro2);
+            Console.WriteLine($"La lista contiene {listaDiLibri.Count} libri");
+
+            listaDiLibri.Remove(libro1);
+            Console.WriteLine($"La lista contiene {listaDiLibri.Count} libri");
+
+            #endregion
+
+
+
+
+
         }
     }
 }
