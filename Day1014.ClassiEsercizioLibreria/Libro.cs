3931a2a844d9fd2bfa19794816f509970cbe1b32
Laura Gagliani <gaglianilaura@gmail.com>
2021-10-14 09:53:25 +0200

    inizio esercizio classi

---
+++ Day1014.ClassiEsercizioLibreria/Libro.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Un libro è un'entità che ha
+//Codice
+//Titolo
+//Autore
+//Genere
+//Prezzo
+//Data di pubblicazione
+
+namespace Day1014.ClassiEsercizioLibreria
+{
+    public class Libro //consiglio di mettere public (come dire "visibile") davanti alla parola class
+    {
+
+
+        //qui dobbiamo scrivere le proprietà che vogliamo dare alla classe: "prop + doppio TAB" e inizia la struttura
+
+        public string Codice { get; set; }  //le proprietà si chiamano con la Maiuscola
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public Genere Genere { get; set; }  //vogliamo usare l'enum: così gli abbiamo detto che il tipo è "Genere" (che devo definire io!!)
+        public double Prezzo { get; set; }
+        public DateTime DataPubblicazione { get; set; }
+
+        //oltre alle Proprietà va messo anche un Costruttore, ovvero uno strumento che serve per costruire oggetti Libro:
+
+        public Libro() //si chiama come la classe; non prende niente e non dà niente
+        {
+
+        }
+
+
+    }
+
+    public enum Genere
+    {
+        Narrativa,      //scrivo una lista di termini a cui lui associa numeri partendo automaticamente da 0
+        Storico,        //posso anche forzare una numerazione a piacere
+        Fantasy,
+        Saggistica,
+        Classici
+    }
+}
---
+++ Day1014.ClassiEsercizioLibreria/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day1014.ClassiEsercizioLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region traccia
+            //Scrivere un programma che gestisca una libreria.
+
+            //Un libro è un'entità che ha
+            //Codice
+            //Titolo
+            //Autore
+            //Genere
+            //Prezzo
+            //Data di pubblicazione
+
+            //Per il genere usare un enum
+
+            //Sarà possibile inserire un nuovo libro, eliminare un libro, modificare un libro o cercare i libri per genere
+            #endregion
+
+            Libro libroTot = new Libro(); //dove Libro() è il costruttore.
+                                          //io sto dicendo costruiscimi una oggetto esempioDiLibro del "tipo" Libro, la classe che ho costruito
+                                          //il costruttore prepara un pezzo di memoria per metterci tutti i dati che abbiamo preparato per la classe
+
+            libroTot.Codice = "Codice001alfanumerico"; //nome.Proprietà
+            libroTot.Autore = "Collodi";
+            libroTot.Titolo = "Pinocchio";
+            libroTot.Prezzo = 12.20;
+            libroTot.DataPubblicazione = new DateTime(1880);
+        }
+    }
+}
